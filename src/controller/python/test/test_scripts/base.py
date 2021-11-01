4ff671b78593371af516e339a6f0d50ed5074639
Cecille Freeman <cecille@google.com>
2021-11-01 14:59:39 -0400

    Address request from #10771
    There was a request to add a timeout to the join in #10771.
    The thread itself is unlikely to hang - more likely situation here
    is the subscription failing to cause updates, causing the main
    thread to hang forever on the condition variable. Added time outs
    to both so we can have a bit more information about what actually
    happened when the test completes.

--- src/controller/python/test/test_scripts/base.py
+++ src/controller/python/test/test_scripts/base.py
@@ -368,12 +368,25 @@ class BaseTestHelper:
             # Reset the number of subscriptions received as subscribing causes a callback.
             handler.subscriptionReceived = 0
             changeThread.start()
+            subscriptionOK = True
             with handler.cv:
                 while handler.subscriptionReceived < 5:
-                    # We should observe 10 attribute changes
-                    handler.cv.wait()
-            changeThread.join()
-            return True
+                    # We should observe 5 attribute changes
+                    # The changing thread will change the value after 3 seconds. If we're waiting more than 10, assume something
+                    # is really wrong and bail out here with some information.
+                    if not handler.cv.wait(10.0):
+                        self.logger.error(f"Failed to receive subscription update")
+                        subsciptionOk = False
+                        break
+
+            # thread changes 5 times, and sleeps for 3 seconds in between. Add an additional 3 seconds of slack. Timeout is in seconds.
+            changeThread.join(18.0)
+            if changeThread.is_alive():
+                # Thread join timed out
+                self.logger.error(f"Failed to join change thread")
+                return False
+            return subsciptionOk
+
         except Exception as ex:
             self.logger.exception(f"Failed to finish API test: {ex}")
             return False
