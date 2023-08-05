7616e39f58b8d8209158934775fa2311c90674f1
René Josefsen <rjosefsen@grundfos.com>
2023-08-05 04:16:42 +0200

    Added client cluster and some base device verification

--- src/python_testing/TC_DeviceTypeValidation.py
+++ src/python_testing/TC_DeviceTypeValidation.py
@@ -25,6 +25,8 @@ from rich.console import Console
 
 console = None
 
+simpleDeviceTypes = [771]
+
 
 def getPicsElementId(picsStr, elementTypeStr, elementIdLen):
     elementIdStrIndex = picsStr.find(elementTypeStr) + len(elementTypeStr)
@@ -42,7 +44,7 @@ class TC_DeviceTypeValidation(MatterBaseTest):
         attributePicsStr = ".S.A"
         attributeIdSize = 4
         commandPicsStr = ".S.C"
-        commandIdSize = 2                        
+        commandIdSize = 2
 
         # Endpoint define
         rootNodeEndpointID = 0
@@ -55,7 +57,8 @@ class TC_DeviceTypeValidation(MatterBaseTest):
         devCtrl = self.default_controller
 
         # Device Type dict
-        deviceTypeDict = {}
+        deviceTypeRequirementFileDict = {}
+        deviceTypeFromDeviceDict = {}
 
         # Store the device type requirement JSON list
         deviceTypeRequirementFileList = os.listdir(deviceTypeRequirementInputPathStr)
@@ -71,15 +74,30 @@ class TC_DeviceTypeValidation(MatterBaseTest):
 
                 # console.print(f"{deviceTypeID} - {int(deviceTypeID, 16)} - {deviceTypeRequirementFileName}")
 
-                deviceTypeDict[int(deviceTypeID, 16)] = {
+                deviceTypeRequirementFileDict[int(deviceTypeID, 16)] = {
                     "fileName": deviceTypeRequirementFileName,
                 }
+        # console.print(deviceTypeRequirementFileDict)
 
-        # console.print(deviceTypeDict)
-        partsListResponse = await devCtrl.ReadAttribute(self.dut_node_id, [(rootNodeEndpointID, Clusters.Descriptor.Attributes.PartsList)])
-        partsList = partsListResponse[0][Clusters.Descriptor][Clusters.Descriptor.Attributes.PartsList]
+        # Determine if any endpoint are duplicate, which is needed for base device requirement verification
+        wildcardDescriptorResponse = await devCtrl.ReadAttribute(self.dut_node_id, [(Clusters.Descriptor.Attributes.DeviceTypeList), (Clusters.Descriptor.Attributes.PartsList)])
+        # console.print(wildcardDescriptorResponse)
 
-        # Add endpoint 0 to the parts list, since this is not returned by the device
+        for endpoint in wildcardDescriptorResponse:
+            for deviceTypeStruct in wildcardDescriptorResponse[endpoint][Clusters.Descriptor][Clusters.Descriptor.Attributes.DeviceTypeList]:
+                if deviceTypeStruct.deviceType not in deviceTypeFromDeviceDict:
+                    deviceTypeFromDeviceDict[deviceTypeStruct.deviceType] = [endpoint]
+                else:
+                    deviceTypeFromDeviceDict[deviceTypeStruct.deviceType].append(endpoint)
+        # console.print(deviceTypeFromDeviceDict)
+
+        # Get the parts list of endpoint 0 to determine which endpoint to validate
+        partsList = wildcardDescriptorResponse[rootNodeEndpointID][Clusters.Descriptor][Clusters.Descriptor.Attributes.PartsList]
+
+        # Verify that parts list on endpoint 0 is not empty
+        asserts.assert_true(partsList, "PartsList is empty ❌")
+
+        # Add endpoint 0 to the parts list, since this is not returned by the device, to validate root node as well
         partsList.insert(0, 0)
 
         console.print(f"PartsList: {partsList}")
@@ -93,12 +111,19 @@ class TC_DeviceTypeValidation(MatterBaseTest):
 
             for deviceTypeStruct in deviceTypeList:
                 console.print(f"[blue]DeviceType: {deviceTypeStruct.deviceType}")
+                console.print(f"[blue]Revision: {deviceTypeStruct.revision}")
 
-                file = f"{deviceTypeRequirementInputPathStr}{deviceTypeDict[deviceTypeStruct.deviceType]['fileName']}"
+                file = f"{deviceTypeRequirementInputPathStr}{deviceTypeRequirementFileDict[deviceTypeStruct.deviceType]['fileName']}"
                 # console.print(file)
 
                 with open(file, 'r') as deviceTypeRequirementFile:
                     deviceTypeRequirementData = json.load(deviceTypeRequirementFile)
+
+                    # Revision
+                    requiredRevision = int(deviceTypeRequirementData['revision'])
+                    console.print(f"Mandated device type revision: {requiredRevision}")
+                    asserts.assert_equal(requiredRevision, deviceTypeStruct.revision, "DeviceType revision does not match ❌")
+                    console.print("[green]Revision validation successful ✅")
 
                     # Server clusteres
                     requiredServerClusters = deviceTypeRequirementData['server_clusters']
@@ -114,11 +139,11 @@ class TC_DeviceTypeValidation(MatterBaseTest):
                     for cluster in requiredServerClusters:
                         # Check if server cluster is available
                         clusterId = int(cluster['id'], 16)
-                        console.print(f"Mandated cluster: {clusterId}")
+                        console.print(f"Mandated server cluster: {clusterId}")
 
                         # Check if cluster id is in the server list
                         asserts.assert_true(clusterId in serverList, f"Cluster ({cluster['id']}) not found in server list ❌")
-                        console.print("[green]Cluster check passed ✅")
+                        console.print("[green]Cluster validation successful ✅")
 
                         clusterClass = getattr(Clusters, devCtrl.GetClusterHandler().GetClusterInfoById(clusterId)['clusterName'])
                         # console.print(devCtrl.GetClusterHandler().GetClusterInfoById(clusterId))
@@ -142,14 +167,14 @@ class TC_DeviceTypeValidation(MatterBaseTest):
                             # Check if mandated feature is supported
                             for feature in mandatoryFeatures:
                                 console.print(f"Mandated feature PICS: {feature['pics_code']}")
-                                
+
                                 featureBitId = getPicsElementId(feature['pics_code'], featurePicsStr, featureIdSize)
 
                                 featureBitState = (featureMap >> featureBitId) & 1
                                 console.print(f"[blue]FeatureBit ({featureBitId}) state {featureBitState}")
 
                                 asserts.assert_true(featureBitState == 1, f"Mandated feature ({featureBitId:#04x}) not found in feature map ❌")
-                                console.print("[green]Feature check passed ✅")
+                                console.print("[green]Feature validation successful ✅")
 
                             for feature in excludedFeatures:
                                 console.print(f"Excluded feature PICS: {feature['pics_code']}")
@@ -158,12 +183,12 @@ class TC_DeviceTypeValidation(MatterBaseTest):
                                     featureBitId = int(feature["id"], 16)
                                 except KeyError:
                                     featureBitId = getPicsElementId(feature['pics_code'], featurePicsStr, featureIdSize)
-                                
+
                                 featureBitState = (featureMap >> featureBitId) & 1
                                 console.print(f"[blue]FeatureBit ({featureBitId}) state {featureBitState}")
 
                                 asserts.assert_true(featureBitState == 0, f"Excluded feature ({featureBitId:#04x}) found in feature map ❌")
-                                console.print("[green]Feature check passed ✅")
+                                console.print("[green]Feature validation successful ✅")
 
                         mandatoryAttributes = cluster["mandatory_attributes"]
 
@@ -186,7 +211,7 @@ class TC_DeviceTypeValidation(MatterBaseTest):
 
                                 console.print(f"[blue]AttributeID: {attributeId}")
                                 asserts.assert_true(attributeId in attributeList, f"Mandated attribute ({attributeId:#06x}) not found in attribute list ❌")
-                                console.print("[green]Attribute check passed ✅")
+                                console.print("[green]Attribute validation successful ✅")
 
                             for attribute in excludedAttributes:
                                 console.print(f"Excluded attribute PICS: {attribute['pics_code']}")
@@ -198,8 +223,8 @@ class TC_DeviceTypeValidation(MatterBaseTest):
 
                                 console.print(f"[blue]AttributeID: {attributeId}")
                                 asserts.assert_true(attributeId not in attributeList, f"Excluded attribute ({attributeId:#06x}) found in attribute list ❌")
-                                console.print("[green]Attribute check passed ✅")
-                                    
+                                console.print("[green]Attribute validation successful ✅")
+
                         mandatoryCommands = cluster["mandatory_commands"]
 
                         # Since excluded_commands might not be in all files, if not present, assume empty
@@ -226,10 +251,10 @@ class TC_DeviceTypeValidation(MatterBaseTest):
 
                                 if ".Rsp" in command['pics_code']:
                                     asserts.assert_true(commandId in acceptedCommandList, f"Mandated command ({commandId:#04x}) not found in accepted command lists ❌")
-                                    console.print("[green]Accepted command check passed ✅")
+                                    console.print("[green]Accepted command validation successful ✅")
                                 elif ".Tx" in command['pics_code']:
                                     asserts.assert_true(commandId in generatedCommandList, f"Mandated command ({commandId:#04x}) not found in generated command lists ❌")
-                                    console.print("[green]Generated command check passed ✅")
+                                    console.print("[green]Generated command validation successful ✅")
                                 else:
                                     asserts.assert_true(False, "[red]Invalid PICS code ❌")
 
@@ -245,13 +270,91 @@ class TC_DeviceTypeValidation(MatterBaseTest):
 
                                 if ".Rsp" in command['pics_code']:
                                     asserts.assert_true(commandId not in acceptedCommandList, f"Excluded command ({commandId:#04x}) found in accepted command lists ❌")
-                                    console.print("[green]Accepted command check passed ✅")
+                                    console.print("[green]Accepted command validation successful ✅")
                                 elif ".Tx" in command['pics_code']:
                                     asserts.assert_true(commandId not in generatedCommandList, f"Excluded command ({commandId:#04x}) found in generated command lists ❌")
-                                    console.print("[green]Generated command check passed ✅")
+                                    console.print("[green]Generated command validation successful ✅")
                                 else:
                                     asserts.assert_true(False, "[red]Invalid PICS code ❌")
 
+                    # Client clusters
+                    requiredClientClusters = deviceTypeRequirementData['client_clusters']
+                    # console.print(requiredClientClusters)
+
+                    # If any mandated clients, read client list and validate
+                    if requiredClientClusters:
+                        # Read client list
+                        clientListResponse = await devCtrl.ReadAttribute(self.dut_node_id, [(endpoint, Clusters.Descriptor.Attributes.ClientList)])
+                        clientList = clientListResponse[endpoint][Clusters.Descriptor][Clusters.Descriptor.Attributes.ClientList]
+                        console.print(f"Client List: {clientList}")
+
+                        for cluster in requiredClientClusters:
+                            # Check if client cluster is available
+                            clusterId = int(cluster['id'], 16)
+                            console.print(f"Mandated client cluster: {clusterId}")
+
+                            # Check if cluster id is in the server list
+                            asserts.assert_true(clusterId in clientList, f"Cluster ({cluster['id']}) not found in client list ❌")
+                            console.print("[green]Cluster validation successful ✅")
+
+                    # --- Verify Base Device Type requirements
+
+                    # Descriptor is mandatory as a part of base device
+                    descriptorClusterId = devCtrl.GetClusterHandler().ListClusterInfo()['Descriptor']['clusterId']
+                    console.print(f"Mandated base device cluster: {descriptorClusterId}")
+                    asserts.assert_true(descriptorClusterId in serverList, f"Descriptor cluster ({descriptorClusterId:#04x}) not found in server list ❌")
+                    console.print("[green]Cluster validation successful ✅")
+
+                    # If device type is a simple device and client is supported, binding is mandatory
+                    bindingClusterId = devCtrl.GetClusterHandler().ListClusterInfo()['Binding']['clusterId']
+
+                    # Determine if clientlist has been read else do it now
+                    try:
+                        clientListForBaseDevice = clientList
+                    except UnboundLocalError:
+                        # Read client list
+                        clientListResponse = await devCtrl.ReadAttribute(self.dut_node_id, [(endpoint, Clusters.Descriptor.Attributes.ClientList)])
+                        clientListForBaseDevice = clientListResponse[endpoint][Clusters.Descriptor][Clusters.Descriptor.Attributes.ClientList]
+                        # console.print(f"Client List: {clientListForBaseDevice}")
+
+                    # Check if binding is present is both conditions are true
+                    if (deviceTypeStruct.deviceType in simpleDeviceTypes) and clientListForBaseDevice:
+                        console.print(f"Mandated base device cluster: {bindingClusterId}")
+                        asserts.assert_true(bindingClusterId in serverList, f"Binding cluster ({bindingClusterId:#04x}) not found in server list ❌")
+                        console.print("[green]Cluster validation successful ✅")
+
+                    # If identify cluster is present, verify that QRY feature is not supported
+                    identifyClusterId = devCtrl.GetClusterHandler().ListClusterInfo()['Identify']['clusterId']
+                    if identifyClusterId in serverList:
+                        console.print(f"Excluded base device feature QRY in cluster: {identifyClusterId}")
+
+                        featureMapResponse = await devCtrl.ReadAttribute(self.dut_node_id, [(endpoint, Clusters.Identify.Attributes.FeatureMap)])
+                        featureMap = featureMapResponse[endpoint][Clusters.Identify][Clusters.Identify.Attributes.FeatureMap]
+                        # console.print(f"FeatureMap: {featureMap} / {featureMap:#010b}")
+
+                        # QRY feature at index 0
+                        featureBitId = 0
+                        featureBitState = (featureMap >> featureBitId) & 1
+                        # console.print(f"[blue]FeatureBit ({featureBitId}) state {featureBitState}")
+                        asserts.assert_true(featureBitState == 0, f"Excluded base device QRY feature ({featureBitId:#04x}) found in feature map ❌")
+                        console.print("[green]Feature validation successful ✅")
+
+                    # If an endpoint’s device type is a duplicated on another endpoint, verify descriptor support TagList feature
+                    for deviceTypeOnEndpoint in deviceTypeFromDeviceDict:
+                        if (endpoint in deviceTypeFromDeviceDict[deviceTypeOnEndpoint]) and len(deviceTypeFromDeviceDict[deviceTypeOnEndpoint]) > 1:
+                            console.print(f"Mandated base device feature TAGLIST in cluster: {descriptorClusterId}")
+
+                            featureMapResponse = await devCtrl.ReadAttribute(self.dut_node_id, [(endpoint, Clusters.Descriptor.Attributes.FeatureMap)])
+                            featureMap = featureMapResponse[endpoint][Clusters.Descriptor][Clusters.Descriptor.Attributes.FeatureMap]
+                            console.print(f"FeatureMap: {featureMap} / {featureMap:#010b}")
+
+                            # TagList feature at index 0
+                            featureBitId = 0
+                            featureBitState = (featureMap >> featureBitId) & 1
+                            # console.print(f"[blue]FeatureBit ({featureBitId}) state {featureBitState}")
+                            asserts.assert_true(featureBitState == 1, f"Mandated base device TAGLIST feature ({featureBitId:#04x}) not found in feature map ❌")
+                            console.print("[green]Feature validation successful ✅")
+                    
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--device-type-data', required=True)
